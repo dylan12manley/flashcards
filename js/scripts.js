5e4fa8213948370084af4397748fc30d90d3f902
Dylan Manley and Lela Smith <dylan@>
2019-10-22 15:24:00 -0700

    Add all term definitions in html, add css styling, add JS functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,82 @@
 $(document).ready(function() {
-  $("h2").click(function() {
-    $(".definition").toggle();
-    $("h2").toggle();
+  $("#JS").click(function() {
+    $("#JS p").toggle();
+    $("#JS h2").toggle();
+  });
+  $("#operators").click(function() {
+    $("#operators p").toggle();
+    $("#operators h2").toggle();
+  });
+  $("#variables").click(function() {
+    $("#variables p").toggle();
+    $("#variables h2").toggle();
+  });
+  $("#vnc").click(function() {
+    $("#vnc p").toggle();
+    $("#vnc h2").toggle();
+  });
+  $("#functions").click(function() {
+    $("#functions p").toggle();
+    $("#functions h2").toggle();
+  });
+  $("#methods").click(function() {
+    $("#methods p").toggle();
+    $("#methods h2").toggle();
+  });
+  $("#arguments").click(function() {
+    $("#arguments p").toggle();
+    $("#arguments h2").toggle();
+  });
+  $("#parameters").click(function() {
+    $("#parameters p").toggle();
+    $("#parameters h2").toggle();
+  });
+  $("#return").click(function() {
+    $("#return p").toggle();
+    $("#return h2").toggle();
+  });
+  $("#chaining-methods").click(function() {
+    $("#chaining-methods p").toggle();
+    $("#chaining-methods h2").toggle();
+  });
+  $("#strings").click(function() {
+    $("#strings p").toggle();
+    $("#strings h2").toggle();
+  });
+  $("#booleans").click(function() {
+    $("#booleans p").toggle();
+    $("#booleans h2").toggle();
+  });
+  $("#undefined").click(function() {
+    $("#undefined p").toggle();
+    $("#undefined h2").toggle();
+  });
+  $("#nan").click(function() {
+    $("#nan p").toggle();
+    $("#nan h2").toggle();
+  });
+  $("#escape").click(function() {
+    $("#escape p").toggle();
+    $("#escape h2").toggle();
+  });
+  $("#alert").click(function() {
+    $("#alert p").toggle();
+    $("#alert h2").toggle();
+  });
+  $("#comments").click(function() {
+    $("#comments p").toggle();
+    $("#comments h2").toggle();
+  });
+  $("#jQuery").click(function() {
+    $("#jQuery p").toggle();
+    $("#jQuery h2").toggle();
+  });
+  $("#attributes").click(function() {
+    $("#attributes p").toggle();
+    $("#attributes h2").toggle();
+  });
+  $("#property").click(function() {
+    $("#property p").toggle();
+    $("#property h2").toggle();
   });
 });
